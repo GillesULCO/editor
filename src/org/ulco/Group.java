11356a927167405489665ff6bd55858d1495b031
gmachure <gmachure@adexos.fr>
2017-11-16 13:30:34 +0100

    refactoring 4eme tache

--- src/org/ulco/Group.java
+++ src/org/ulco/Group.java
@@ -8,7 +8,7 @@ public class Group extends GraphicsObject {
 
     public Group() {
         m_objectList = new Vector<GraphicsObject>();
-        m_ID = ++ID.ID;
+        m_ID = ID.getInstance().getNextId();
     }
 
     public Group(String json) {
--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,24 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    private static ID instance = null;
+    static public int id;
+
+    private ID(){
+        this.id = 0;
+    }
+
+    public static ID getInstance() {
+        if(instance == null)
+            instance = new ID();
+        return instance;
+    }
+
+    public int getNextId() {
+        return ++this.id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 }
